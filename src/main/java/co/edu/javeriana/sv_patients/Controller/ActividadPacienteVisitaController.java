ef4e99d7d8d4b15a2912a9e99fd0d315f13d8edf
linasalamanca <118871999+linasalamanca@users.noreply.github.com>
2025-04-23 08:55:23 -0500

    actividad paciente visita

---
+++ src/main/java/co/edu/javeriana/sv_patients/Controller/ActividadPacienteVisitaController.java
@@ -0,0 +1,73 @@
+package co.edu.javeriana.sv_patients.Controller;
+
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import co.edu.javeriana.sv_patients.Entity.ActividadPacienteVisita;
+import co.edu.javeriana.sv_patients.Service.ActividadPacienteVisitaService;
+
+@RestController
+@RequestMapping("/actividad-paciente-visita")
+public class ActividadPacienteVisitaController {
+    @Autowired
+    private ActividadPacienteVisitaService actividadPacienteVisitaService;
+
+
+    //http://localhost:8081/actividad-paciente-visita/registrar
+    @PostMapping("/registrar")
+    public ResponseEntity<?> registrarActividad(@RequestBody Map<String, Object> payload) {
+        try {
+            ActividadPacienteVisita actividad = actividadPacienteVisitaService.crearDesdePayload(payload);
+            return ResponseEntity.status(HttpStatus.CREATED).body(actividad);
+        } catch (RuntimeException e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+        }
+    }
+
+    //http://localhost:8081/actividad-paciente-visita/listar
+    @GetMapping("/listar")
+    public ResponseEntity<List<Map<String, Object>>> listarActividadesSoloIds() {
+        List<Map<String, Object>> actividades = actividadPacienteVisitaService.obtenerActividadesSoloConIds();
+        return ResponseEntity.ok(actividades);
+    }
+
+
+    //http://localhost:8081/actividad-paciente-visita/listar/1
+    @GetMapping("/listar/{id}")
+    public ResponseEntity<ActividadPacienteVisita> obtenerActividadPorId(@PathVariable Long id) {
+        ActividadPacienteVisita actividad = actividadPacienteVisitaService.obtenerActividadPacienteVisitaPorId(id);
+        if (actividad != null) {
+            return ResponseEntity.ok(actividad);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @PutMapping("/actualizar/{id}")
+    public ResponseEntity<ActividadPacienteVisita> actualizarActividad(@PathVariable Long id, @RequestBody ActividadPacienteVisita actividadPacienteVisitaActualizada) {
+        ActividadPacienteVisita actividadActualizada = actividadPacienteVisitaService.actualizarActividadPacienteVisita(id, actividadPacienteVisitaActualizada);
+        if (actividadActualizada != null) {
+            return ResponseEntity.ok(actividadActualizada);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @DeleteMapping("/eliminar/{id}")
+    public ResponseEntity<Void> eliminarActividad(@PathVariable Long id) {
+        actividadPacienteVisitaService.eliminarActividadPacienteVisita(id);
+        return ResponseEntity.noContent().build();
+    }
+}
